23c2dd02b9cfcf8b6fb64d4b683459eeb396687c
Jan Christian <janc2@uci.edu>
2018-09-29 13:03:33 -0700

    Replace generateTournament.sh

--- WorldGenerator/generateTournament.sh
+++ WorldGenerator/generateTournament.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-rm -rf Worlds
-mkdir Worlds
+rm -rf Problems
+mkdir Problems
+cd Problems
+mkdir Beginner
+mkdir Intermediate
+mkdir Expert
+cd ..
+ 
+python3 WorldGenerator.py 1000 Beginner_world_ 8 8 10
 
-python3 WorldGenerator.py 1000 beginner_world_ 8 8 10
-python3 WorldGenerator.py 1000 intermediate_world_ 16 16 40
-python3 WorldGenerator.py 1000 expert_world_ 16 30 99
+python3 WorldGenerator.py 1000 Intermediate_world_ 16 16 40
+
+python3 WorldGenerator.py 1000 Expert_world_ 16 30 99
 
 echo Finished generating worlds!
