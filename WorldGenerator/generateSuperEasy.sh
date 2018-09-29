d40db97521e739163e29087a2cb4dcc3042a5c2c
Jan Christian <janc2@uci.edu>
2018-09-29 13:03:16 -0700

    Replace generateSuperEasy.sh

--- WorldGenerator/generateSuperEasy.sh
+++ WorldGenerator/generateSuperEasy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf Worlds
-mkdir Worlds
+rm -rf Problems
+mkdir Problems
 
-python3 WorldGenerator.py 1000 super_easy_world_ 5 5 1
+python3 WorldGenerator.py 1000 Easy_world_ 5 5 1
 
 echo Finished generating worlds!
