73b9663321726d60301f3306350391bac6cfb369
98penaloza <47442557+98penaloza@users.noreply.github.com>
2021-05-03 15:41:03 -0700

    New version

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -15,9 +15,14 @@ from AI import AI
 from Action import Action
 
 class Board:
-	def __init__(self, arr, currentXY):
-		self.__board = arr
-		self.size = len(arr)
+	def __init__(self, row, col, totalMines, currentXY, cover = '.', bomb = 'B'):
+		self.colDim = col
+		self.rowDim = row
+		self.totalMines = totalMines
+		self.totalTiles = row * col
+		self.cover = cover
+		self.bomb = bomb
+		self.__board = [[self.cover for i in range(self.colDim)] for j in range(self.rowDim)]
 		self.x, self.y  = currentXY
 
 	
@@ -36,6 +41,45 @@ class Board:
 		return s
 
 
+	def getTileAt(self, x, y):
+		return self.__board[y][x]
+
+	def incTileAt(self, x, y, val):
+		self.__board[y][x] += val
+	
+	def decTileAt(self, x, y, val):
+		self.__board[y][x] -= val
+
+	def setTileAt(self, x, y, val):
+		self.totalTiles -= 1
+		if val < 0:
+			self.totalMines -= 1
+		self.__board[y][x] = val if val >=0 else self.bomb
+
+	def iterAt(self, x, y, match = None):
+		for row in [-1, 0, 1]:
+			for col in [-1, 0, 1]:
+				if not (row == col == 0)\
+				and 0 <= x+row < self.rowDim\
+				and 0 <= y+col < self.colDim:
+					val = self.getTileAt(x+row, y+col)
+					if match == None:
+						yield x+row, y+col, val
+					else:
+						if match == val:
+							yield x+row, y+col, val
+
+
+
+	def isBombAt(self, x, y):
+		return self.getTileAt(x,y) == self.bomb
+	
+	def isCoveredAt(self, x, y):
+		return self.getTileAt(x,y) == self.cover
+
+
+
+
 
 class MyAI( AI ):
 
@@ -46,16 +90,15 @@ class MyAI( AI ):
 		
 		# Test by using:
 		#		py main.py -d
-		self.__rowDim = rowDimension
-		self.__colDim = colDimension
-		self.__totalTiles = rowDimension * colDimension
-		self.__uncensored = "."
-
+		# self.__rowDim = rowDimension
+		# self.__colDim = colDimension
+		# self.__totalTiles = rowDimension * colDimension
+		# self.__uncensored = "."
+		# self.__bomb = 'B'
 		# self.__board = [['N' for i in range(self.__colDim)] for j in range(self.__rowDim)]  # Not sure whether to keep this
-		self.__board = Board([[ self.__uncensored for i in range(self.__colDim)] for j in range(self.__rowDim)], (startX, startY))
+		self.__board = Board(rowDimension, colDimension, totalMines, (startX, startY))
 		self.__lastX = startX
 		self.__lastY = startY
-		self.__totalMines = totalMines
 		self.__tileNumState = None
 		self.__isGameOver = False
 		self.__safeMoves = []
@@ -75,16 +118,17 @@ class MyAI( AI ):
 		########################################################################
 		self.__tileNumState = number
 		self.__updateGame()
-		# print(self.__board)
+		print(self.__board)
 		# print("Visited: ", self.__visited)
 		# print("SafeMoves: ", self.__safeMoves)
 		# print("flagCoor: ", self.__flagCoor)
 		# print("Bombs ", self.__totalMines)
 		if not self.__isGameOver:
-			self.__findSafeMoves()
+			self.__findSafeMoves(self.__lastX, self.__lastY)
 			if (len(self.__safeMoves)):
 				action = AI.Action.UNCOVER
 				x, y = self.__getMove()
+				# print(x,y)
 				return Action(action, x, y)
 
 			else:
@@ -93,15 +137,10 @@ class MyAI( AI ):
 					action = AI.Action.FLAG
 					x, y = self.__getMove()
 					self.__visited.append((x, y))
-					self.__totalMines -= 1
 					return Action(action, x, y)
 
 				else:
 					pass
-
-				
-				
-			
 			
 		else:
 			action = AI.Action.LEAVE
@@ -109,7 +148,6 @@ class MyAI( AI ):
 
 			
 			
-
 
 		return Action(AI.Action.LEAVE)
 		########################################################################
@@ -128,50 +166,31 @@ class MyAI( AI ):
 	##		         ----------------				   ##
 	#####################################################
 	def __updateGame(self):
-		# if self.__tileNumState == -1:
-		# 	self.__tileNumState = 'B'
-		# self.__board[self.__lastY][self.__lastX] = self.__tileNumState
-		# if self.__tileNumState != 'B' and self.__tileNumState > 0:
-		# 		self.__tocheck.append(((self.__lastX,self.__lastY), self.__tileNumState))
 		if self.__tileNumState > 0:
-			for row in [-1, 0, 1]:
-					for col in [-1, 0, 1]:
-						if not (row == col == 0) \
-						and 0 <= self.__lastX+row < self.__rowDim and 0 <= self.__lastY+col < self.__colDim \
-						and self.__getTileAt(self.__lastX+row, self.__lastY+col) == "B":
-							self.__tileNumState -= 1
+			for x_temp, y_temp, val in self.__board.iterAt(self.__lastX, self.__lastY, match = self.__board.bomb):
+				self.__tileNumState -= 1
 
-		self.__board[self.__lastY][self.__lastX] = self.__tileNumState if self.__tileNumState >-1 else 'B'
 		
+		self.__board.setTileAt(self.__lastX, self.__lastY, self.__tileNumState)
 		if self.__tileNumState > 0:
-				self.__tocheck.append(((self.__lastX,self.__lastY), self.__tileNumState))
+				self.__tocheck.append((self.__lastX,self.__lastY))
 
 		if self.__tileNumState < 0 :
 			self.subtract()
 
-		self.__totalTiles -= 1
-		if self.__totalMines == self.__totalTiles:
+		if self.__board.totalTiles == 0:
 			self.__isGameOver = True
 		
 
 
+	def __findSafeMoves(self, x, y):
+		val = self.__board.getTileAt(x,y)
+		if val == 0:
+			for xTemp, yTemp, val in self.__board.iterAt(x, y, match = self.__board.cover):
+				if (xTemp, yTemp) not in self.__safeMoves:
+					self.__safeMoves.append((xTemp, yTemp))
 
-	def __getTileAt(self, x, y):
-		val = self.__board[y][x]
-		return val
 
-	def __findSafeMoves(self):
-		if self.__tileNumState == 0:
-			x = self.__lastX
-			y = self.__lastY
-			for i in [-1, 0, 1]:
-				for j in [-1, 0, 1]:
-					if not (i == j == 0 )\
-					 and 0 <= x+i < self.__rowDim\
-					 and 0 <= y+j < self.__colDim\
-					 and self.__getTileAt(x+i, y+j) == self.__uncensored \
-					 and (x+i, y+j) not in self.__safeMoves:
-						self.__safeMoves.append((x+i, y+j))
 
 	def __getMove(self):
 		x = y = 0
@@ -185,121 +204,37 @@ class MyAI( AI ):
 	
 
 	def __findBomb(self):
-		self.__tocheck.sort(key= lambda x: x[1])
+		self.__tocheck.sort(key= lambda coor: self.__board.getTileAt(coor[0], coor[1]))
+		for temp in self.__tocheck[:]:
+			wasBomb = self.__markBombs(temp)
+			if wasBomb:
+				self.__tocheck.remove(temp)
 		
-		while (len(self.__tocheck)):
-			temp = self.__tocheck.pop(0) # ((self.__lastX,self.__lastY), self.__tileNumState)
-			# x, y = temp[0]
-			# tile_int = temp[1]
-			self.__markBombs(temp[0], temp[1])
 
-			# temp_board = self.__makeTempBoard(x, y) #X and Y start at 0
-			
-
-			# number_of_bombs = self.__countBombs(temp_board)
-			# if tile_int == number_of_bombs:
-			# 	self.__bomb_corr(temp_board, (x, y))
-			
-
-	def __markBombs(self, xy, tileNum):
+	def __markBombs(self, xy):
 		x, y = xy
+		tileNum = self.__board.getTileAt(x,y)
 		emptyCoor = set()
-		for row in [-1, 0, 1]:
-			for col in [-1, 0, 1]:
-				if not (row == col == 0) \
-					and 0 <= x+row < self.__rowDim and 0 <= y+col < self.__colDim \
-					and self.__getTileAt(x+row, y+col) == self.__uncensored \
-					and (x+row, y+col) not in emptyCoor:
-						emptyCoor.add((x+row, y+col))
+		wasBomb = False
+		for xTemp, yTemp, val in self.__board.iterAt(x, y, self.__board.cover):
+			emptyCoor.add((xTemp, yTemp))
 
 		if len(emptyCoor) == tileNum:
+			wasBomb = True
 			for coor in emptyCoor:
-				if coor not in self.__flagCoor:
+				if self.__board.getTileAt( coor[0], coor[1]) == self.__board.cover:
 					self.__flagCoor.append(coor)
-			
+		return wasBomb
 
-
-
-
-	# def __bomb_corr(self, temp_board, middle):
-	# 	bombs = []
-	# 	for row in [-1, 0, 1]:
-	# 		for col in [-1, 0 , 1]:
-	# 			if (0 <= 1 + row < temp_board.size and  0 <=  1 + col < len(temp_board[0])):
-	# 				if ((temp_board[1 + row][1 + col]) == self._uncensored):
-	# 					if ((middle[0] + col, middle[1] + row) not in self.__flagCoor):
-	# 						self.__flagCoor.append((middle[0] + col, middle[1] + row))
-
-
-	# def __countBombs(self, temp_board):
-	# 	count = 0
-	# 	for row in temp_board:
-	# 		for tile in row:
-	# 			if tile == self._uncensored:
-	# 				count += 1
-	# 	return count
 						
 	def subtract(self):
 		while (len(self.__visited)):
 			coor = self.__visited.pop(0)
-			temp = self.__makeTempBoard(coor[0],coor[1])
-			for row in [-1, 0, 1]:
-				for col in [-1, 0 , 1]:
-					if (0 <= 1 + row < temp.size and  0 <=  1 + col < len(temp[0])):
-						if (temp[1 + row][1 + col] not in [self.__uncensored, "B", "E", 0]):
-							self.__board[coor[1] + row][coor[0] + col] -= 1
-							# print(self.__board)
-							# print("Bombs ", self.__totalMines)
-							if (self.__board[coor[1] + row][coor[0] + col] == 0):
-
-								# I added this because I think this is what you meant
-								currXY = (self.__lastX, self.__lastY)
-								currTile = self.__tileNumState
-								
-								self.__lastX, self.__lastY = (coor[0] + col, coor[1] + row)
-								self.__tileNumState = 0 
-								self.__findSafeMoves()
-								
-								self.__lastX, self.__lastY = currXY
-								self.__tileNumState = currTile
-								# self.__safeMoves.append((coor[0] + col,coor[1] + row))
-							else:
-								#                   # toCheck.append((x, y), tileVal) 
-								self.__tocheck.append(((coor[1] + row,coor[0] + col) , self.__board[coor[1] + row][coor[0] + col]))
-
-
-						
-
-
-
-			
-
-
-		# Go through each tile and check if the number of uncover tiles in its grid = its tile number
-		# if so the remaining tiles are bombs and should be flagged
-		# save all the flag x,y coor
-		
-		
-		
-				
-
-
-
-	# I think we can skip this part if we think of a tempBoard as a 3x3 board
-	# and use the bigger board under such dimensions
-	def __makeTempBoard(self, x,y):
-		temp = [] 
-		for row in [-1, 0, 1]:
-			tempRow = []
-			for col in [-1, 0 , 1]:
-				if 0 <= x+col < self.__rowDim and 0 <= y+row < self.__colDim:
-					tempRow.append(self.__board[y+row][x+col])
-				else:
-					tempRow.append('E')
-			temp.append(tempRow)
-		tempBoard = Board(temp, (x,y))
-		return tempBoard
-				
-		
-
-
+			for xTemp, yTemp, val in self.__board.iterAt(coor[0], coor[1]):
+				if not self.__board.isBombAt(xTemp, yTemp) and not self.__board.isCoveredAt(xTemp,yTemp) and val>0:
+					self.__board.decTileAt(xTemp, yTemp, 1)
+					newVal = self.__board.getTileAt(xTemp,yTemp) 
+					if newVal == 0:
+						self.__findSafeMoves(xTemp, yTemp)
+					else:
+						self.__tocheck.append((xTemp, yTemp))
